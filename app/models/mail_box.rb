00b3ee040fd4eb18e8efb09ccd2cc80a1464840b
qaohv <qaohv@railscore.local>
2014-06-15 16:10:51 +0700

    add validator to pop3 server field in Mailbox, before mailbox controller test

--- app/models/mail_box.rb
+++ app/models/mail_box.rb
@@ -1,4 +1,5 @@
 class MailBox < ActiveRecord::Base
   validates :login, :pop3_server, :domain, presence: true
+  validates :pop3_server, format: {with: /pop3.*/, message: "pop3 server address should start with pop3"}
   belongs_to :user
 end
