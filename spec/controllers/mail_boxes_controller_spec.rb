381f714a5fd7e15f08dcf8fcc8c2c873b862aede
qaohv <qaohv@railscore.local>
2014-06-22 21:32:38 +0700

    refactoring mail_boxes_controller_spec step 2

--- spec/controllers/mail_boxes_controller_spec.rb
+++ spec/controllers/mail_boxes_controller_spec.rb
@@ -19,16 +19,21 @@ describe Dashboard::MailBoxesController do
         end
       end
     end
+
+    def render_correct_template(*actions)
+      actions.each do |action|
+        it "render template action #{action}" do
+        verb = action == :index ? "GET" : "POST"
+          process action, verb, {}
+        end
+      end
+    end
   end
 
   get_response_status_code_200(:index, :new, :edit)
+  render_correct_template(:index, :create)
 
   describe "index action" do
-    it "render template" do
-      get :index
-      expect(response).to render_template('index')
-    end
-
     it "check instance variables" do
       get :index
       expect(assigns(:mail_boxes)).to match_array(@user.mail_boxes)
@@ -42,28 +47,20 @@ describe Dashboard::MailBoxesController do
         post :create, mail_box: { login: "dns.ryabokon", domain: "google.com", pop3_server: "pop3.google.com" }
       }.to change(MailBox,:count).by(1)
     end
-
-    it "render new page when fail in create action" do
-      post :create, {}
-      expect(response).to render_template('new')
-    end
   end
 
   describe "destroy action" do
     it "delete action" do
-      mail_box = @user.mail_boxes.create!(login: "dns.ryabokon", pop3_server: "pop3.google.com", domain: "google.com")
       expect {
-        delete :destroy, id: mail_box.id
+        delete :destroy, id: @mail_box.id
       }.to change(MailBox,:count).by(-1)
     end
   end
 
-
   describe "edit action" do
     it "check instance variables" do
-      mail_box = @user.mail_boxes.create!(login: "dns.ryabokon", pop3_server: "pop3.google.com", domain: "google.com")
-      get :edit, id: mail_box.id
-      expect(assigns(:mail_box)).to match(mail_box)
+      get :edit, id: @mail_box.id
+      expect(assigns(:mail_box)).to match(@mail_box)
     end
 
     it "render 404 page if object doen't exist" do
