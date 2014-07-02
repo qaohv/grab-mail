b631034cfbaa7b8cae687888695e58ff433cb362
qaohv <qaohv@railscore.local>
2014-07-03 01:05:57 +0700

    add breadcrumbs, need move upload to background and test, need test IR on not exists record and add pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include ApplicationHelper
 
   protected
     def after_sign_in_path_for(current_user)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def breadcrumbs(*args)
+    bc = "<ol class=\"breadcrumb\">"
+    args.flatten.each do |argument|
+      bc += "<li>#{argument}</li>"
+    end
+    bc += "</ol>"
+    bc.html_safe
+  end
 end
